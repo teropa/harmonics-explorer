bd8e6384137658048645d2e767010a9b94f5bfa3
Tero Parviainen <tero@teropa.info>
2016-09-01 17:03:52 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hs-app',
+  template: 'Hi'
+})
+export class AppComponent { }
---
+++ src/vendor.ts
@@ -0,0 +1,8 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+
+// RxJS
+import 'rxjs';
