8e9c67ab611bf836ad7de85b8ec9691c53de5038
Tero Parviainen <tero@teropa.info>
2016-09-02 12:49:45 +0300

    individual harmonic gain control

--- src/app/audio.service.ts
+++ src/app/audio.service.ts
@@ -4,8 +4,13 @@ import { List } from 'immutable';
 import { Store } from '@ngrx/store';
 import { Actions, Effect, mergeEffects } from '@ngrx/effects';
 
-import { START, STOP, CHANGE_MASTER_GAIN } from './harmonics';
+import { START, STOP, CHANGE_MASTER_GAIN, CHANGE_AMPLITUDE } from './harmonics';
 import { AppState } from './app-state';
+
+interface OscillatorBankItem {
+  oscillator: OscillatorNode;
+  gain: GainNode;
+}
 
 @Injectable()
 export class AudioService implements OnDestroy {
@@ -14,7 +19,7 @@ export class AudioService implements OnDestroy {
   audioCtx = new AudioContext();
   masterGain = this.audioCtx.createGain();
 
-  oscillatorBank: List<OscillatorNode>;
+  oscillatorBank: List<OscillatorBankItem>;
 
   constructor(private actions$: Actions, private store: Store<AppState>) { 
     this.subscription = mergeEffects(this).subscribe(store);
@@ -27,29 +32,50 @@ export class AudioService implements OnDestroy {
     .do(([action, state]) => this.start(state));
   @Effect({dispatch: false}) stop$ = this.actions$
     .ofType(STOP)
-    .do(() => this.stop());
+    .withLatestFrom(this.store)
+    .do(([action, state]) => this.stop(state));
   @Effect({dispatch: false}) changeMasterGain$ = this.actions$
     .ofType(CHANGE_MASTER_GAIN)
     .withLatestFrom(this.store.select('masterGain'))
     .do(([action, gain]) => this.setMasterGain(<number>gain));
+  @Effect({dispatch: false}) changeAmplitude$ = this.actions$
+    .ofType(CHANGE_AMPLITUDE)
+    .do(action => this.setGain(action.payload.partial, action.payload.amplitude));
 
   private start(state: AppState) {
-    this.oscillatorBank = <List<OscillatorNode>>state.partials.map(partial => {
+    this.masterGain.gain.setValueAtTime(0, this.audioCtx.currentTime);
+    this.masterGain.gain.linearRampToValueAtTime(state.masterGain, this.audioCtx.currentTime + 0.03);
+    this.oscillatorBank = <List<OscillatorBankItem>>state.partials.map(partial => {
       const oscillator = this.audioCtx.createOscillator();
+      const gain = this.audioCtx.createGain();
+
       oscillator.frequency.value = partial.frequency;
-      oscillator.connect(this.masterGain);
+      gain.gain.value = partial.amplitude;
+
+      oscillator.connect(gain);
+      gain.connect(this.masterGain);
+
       oscillator.start();
-      return oscillator;
+
+      return {oscillator, gain};
     });
   }
 
-  private stop() {
-    this.oscillatorBank.forEach(osc => osc.stop());
+  private stop(state: AppState) {
+    this.masterGain.gain.setValueAtTime(state.masterGain, this.audioCtx.currentTime);
+    this.masterGain.gain.linearRampToValueAtTime(0, this.audioCtx.currentTime + 0.03);
+    this.oscillatorBank.forEach(item => item.oscillator.stop(this.audioCtx.currentTime + 0.03));
     this.oscillatorBank = null;
   }
 
   private setMasterGain(gain: number) {
-    this.masterGain.gain.value = gain;
+    this.masterGain.gain.setValueAtTime(gain, this.audioCtx.currentTime);
+  }
+
+  private setGain(partial: number, gain: number) {
+    if (this.oscillatorBank) {
+      this.oscillatorBank.get(partial).gain.gain.value = gain;
+    }
   }
 
   ngOnDestroy() {
