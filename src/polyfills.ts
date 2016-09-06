94f7ec539fde921a969a214567767ca0fb119b04
Tero Parviainen <tero@teropa.info>
2016-09-06 10:41:45 +0300

    web audio shim for Safari

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,7 @@
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 require('zone.js/dist/zone');
+require("@mohayonao/web-audio-api-shim");
 
 if (process.env.ENV === 'production') {
   // Production
